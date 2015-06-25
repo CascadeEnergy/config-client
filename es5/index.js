40220af335db9497dbee0a59925842ba46971f01
michael schoenfelder <michaelkschoenfelder@gmail.com>
2015-06-25 13:55:24 -0700

    generated module, added config-client code

---
+++ es5/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _gotPromise = require('got-promise');
+
+var _gotPromise2 = _interopRequireDefault(_gotPromise);
+
+function pluckValue(response) {
+  return response.body[0].Value;
+}
+
+function decodeBase64(value) {
+  return new Buffer(value, 'base64').toString();
+}
+
+function factory(host) {
+
+  function get(key) {
+    return (0, _gotPromise2['default'])('http://' + host + '/v1/kv/' + key, { json: true }).then(pluckValue).then(decodeBase64);
+  }
+
+  return {
+    get: get
+  };
+}
+
+exports['default'] = factory;
+module.exports = exports['default'];
---
+++ es6/test/index.js
@@ -0,0 +1,35 @@
+import configFactory from '../index';
+
+import assert from 'assert';
+import nock from 'nock';
+
+
+describe('Config Client', () => {
+  const host = 'key.value.service';
+  const url = 'http://' + host + '/v1/kv/';
+  const key = 'key';
+  const expectedValue = 'value';
+  const encodedValue = new Buffer(expectedValue).toString();
+  let configClient;
+
+  beforeEach(() => {
+    configClient = configFactory(host);
+  });
+
+  it('should retrieve value', () => {
+    nock(url)
+      .get()
+      .reply(200, [{
+        CreateIndex: 97,
+        ModifyIndex: 97,
+        Key: key,
+        Flags: 0,
+        Value: encodedValue
+      }]);
+
+    configClient.get(key)
+      .then(function(actualValue) {
+        assert.equal(actualValue, expectedValue);
+      });
+  });
+});
